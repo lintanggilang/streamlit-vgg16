009ac3d49bcfeb8a76193b51e48d8ec1895331b9
Lintang Gilang Pratama <67200488+lintanggilang@users.noreply.github.com>
2023-09-04 15:09:08 +0700

    vgg16 streamlit
    vgg16 streamlit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np
+from keras.applications import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from PIL import Image
+import cv2
+
+def main():
+    # Load the VGG16 model
+    model = VGG16(weights='imagenet', include_top=True)
+
+    # Set up the Streamlit app
+    st.title('VGG16 Image Classification')
+
+    # Upload an image
+    image = st.file_uploader('Upload an image')
+    if image is not None:
+        image = np.array(Image.open(image))
+        image = cv2.resize(image, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+
+        # Preprocess the image for VGG16
+        image_processed = preprocess_input(np.expand_dims(image, axis=0))
+
+        # Classify the image
+        prediction = model.predict(image_processed)
+
+        # Decode the prediction to get human-readable label
+        decoded_predictions = decode_predictions(prediction, top=1)
+        label = decoded_predictions[0][0][1]
+
+        # Display the classification
+        st.write('Predicted label:', label)
+        st.image(image)
+
+if __name__ == '__main__':
+    main()
