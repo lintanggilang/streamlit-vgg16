e7dce85b9ce23d5d138a7b7f4c7a9de16721f435
Lintang Gilang Pratama <67200488+lintanggilang@users.noreply.github.com>
2023-09-13 00:04:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def main():
         label = decoded_predictions[0][0][1]
 
         # Display the classification
-        st.write('Predicted label:', label)
+        st.subheader('Predicted label:', label)
         st.image(image)
 
 if __name__ == '__main__':
